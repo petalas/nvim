0ac152f98cc22566e841c5b5bd2ee3282cac3ec2
Brandon Clark <bwclark97@gmail.com>
2024-07-28 12:43:08 -0400

    Modify conform comments to prevent deprecation warning when used (#1057)

--- init.lua
+++ init.lua
@@ -838,7 +838,7 @@ require('lazy').setup({
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        javascript = { { 'prettierd', 'prettier' } },
+        javascript = { "prettierd", "prettier", stop_after_first = true },
       },
     },
   },
